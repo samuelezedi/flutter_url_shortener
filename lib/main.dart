5b804db58aedb035c76f6f9f1ba063d69750e9df
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-30 22:46:14 +0100

    added api selector

--- lib/main.dart
+++ lib/main.dart
@@ -25,23 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class FlutterClipboard{
-
-  ///
-  static Future<void> copy(String text) async {
-    Clipboard.setData(ClipboardData(
-        text: text
-    ));
-    return;
-  }
-
-  static Future<String> paste() async {
-    ClipboardData data = await Clipboard.getData('text/plain');
-    return data.text.toString();
-  }
-
-}
-
-
-
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,8 @@
 
+import 'package:clipboard/clipboard.dart';
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_clipboard_manager/flutter_clipboard_manager.dart';
-import 'package:uree/main.dart';
-import 'package:uree/utils/config.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -13,6 +11,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController longUrl = TextEditingController();
+  TextEditingController api = TextEditingController();
+
   int _currentIndex = 0;
 
   _onTapped(int index) {
@@ -20,11 +20,26 @@ class _HomeState extends State<Home> {
       _currentIndex = index;
     });
   }
+
+  getUserData() async {
+    SharedPreferences local = await SharedPreferences.getInstance();
+    if(local.getString('user_api_option') != null){
+      setState(() {
+        api.text = local.getString('user_api_option');
+      });
+    }
+  }
+
+  saveUserApiOption(value) async {
+    SharedPreferences local = await SharedPreferences.getInstance();
+    local.setString('user_api_option', value);
+  }
   
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getUserData();
   }
 
   @override
@@ -45,110 +60,169 @@ class _HomeState extends State<Home> {
 //        ],
 //      ),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: <Widget>[
-            SizedBox(
-              height: 150,
-            ),
-            TextFormField(
-              onChanged: (value){
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                    Color(0xFF5E35B1),
+                    Color(0xFF9575CD),
+                  ],
+                )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 100,
+                    ),
+                    TextFormField(
+                      onChanged: (value){
 
-              },
-              controller: longUrl,
-              style: TextStyle(fontSize: 25),
-              cursorColor: Colors.deepPurple,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(10),
-                hintText: 'Long url',
-                hintStyle: TextStyle(fontSize: 20, color: Colors.grey[400]),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+                      },
+                      readOnly: true,
+                      controller: api,
+                      style: TextStyle(fontSize: 25,color: Colors.grey[700]),
+                      cursorColor: Colors.deepPurple,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: (){
+                            selectApiDialog(context);
+                          },
+                          icon: Icon(Icons.keyboard_arrow_down),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding: EdgeInsets.all(10),
+                        hintText: 'Api',
+                        hintStyle: TextStyle(fontSize: 20, color: Colors.grey[400]),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 2)),
+                        labelStyle: TextStyle(color: Colors.black),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 2)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    TextFormField(
+                      onChanged: (value){
+
+                      },
+                      controller: longUrl,
+                      style: TextStyle(fontSize: 25,color: Colors.grey[700]),
+                      cursorColor: Colors.deepPurple,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding: EdgeInsets.all(10),
+                        hintText: 'Long url',
+                        hintStyle: TextStyle(fontSize: 20, color: Colors.grey[400]),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 2)),
+                        labelStyle: TextStyle(color: Colors.black),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white, width: 2)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        RaisedButton(
+                          onPressed: () async {
+                            var data = await FlutterClipboard.paste();
+                            longUrl.text = data.toString();
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0)),
+                          padding: const EdgeInsets.all(0.0),
+                          child: Ink(
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                  Color(0xFF5E35B1),
+                                  Color(0xFF9575CD),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                            ),
+                            child: Container(
+//                      constraints: const BoxConstraints(minWidth: 88.0, minHeight: 36.0),// min sizes for Material buttons
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(10),
+                              width: 100,
+                              height: 45,
+                              child: Text(
+                                'PASTE',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                        RaisedButton(
+                          onPressed: () {
+                            FlutterClipboard.copy(longUrl.text).then((value) => print('copied'));
+                          },
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0)),
+                          padding: const EdgeInsets.all(0.0),
+                          child: Ink(
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                  Color(0xFF5E35B1),
+                                  Color(0xFF9575CD),
+                                ],
+                              ),
+                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                            ),
+                            child: Container(
+//                      constraints: const BoxConstraints(minWidth: 88.0, minHeight: 36.0),// min sizes for Material buttons
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.all(10),
+                              width: 100,
+                              height: 45,
+                              child: Text(
+                                'SHORTEN',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
                 ),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
-                labelStyle: TextStyle(color: Colors.black),
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
               ),
             ),
-            SizedBox(
-              height: 12,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () async {
-                    var data = await FlutterClipboard.paste();
-                    longUrl.text = data.toString() +"--";
-                  },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
-                  padding: const EdgeInsets.all(0.0),
-                  child: Ink(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                        colors: [
-                          Color(0xFF5E35B1),
-                          Color(0xFF9575CD),
-                        ],
-                        stops: [0.1, 0.4],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    ),
-                    child: Container(
-//                      constraints: const BoxConstraints(minWidth: 88.0, minHeight: 36.0),// min sizes for Material buttons
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(10),
-                      width: 100,
-                      height: 45,
-                      child: Text(
-                        'PASTE',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    FlutterClipboard.copy(longUrl.text).then((value) => print('copied'));
-                  },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
-                  padding: const EdgeInsets.all(0.0),
-                  child: Ink(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                        colors: [
-                          Color(0xFF5E35B1),
-                          Color(0xFF9575CD),
-                        ],
-                        stops: [0.1, 0.4],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    ),
-                    child: Container(
-//                      constraints: const BoxConstraints(minWidth: 88.0, minHeight: 36.0),// min sizes for Material buttons
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.all(10),
-                      width: 100,
-                      height: 45,
-                      child: Text(
-                        'SHORTEN',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                )
-              ],
+
+            Expanded(
+              child: ListView.builder(
+
+              ),
             )
+
           ],
         ),
       ),
@@ -161,8 +235,125 @@ class _HomeState extends State<Home> {
             BottomNavigationBarItem(
                 title: Text('Home'), icon: Icon(FeatherIcons.home)),
             BottomNavigationBarItem(
-                title: Text('Favourite'), icon: Icon(FeatherIcons.star)),
+                title: Text('More'), icon: Icon(FeatherIcons.menu)),
           ]),
+    );
+  }
+
+  void selectApiDialog(BuildContext context) {
+    showDialog(context: context,
+      builder: (context){
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15)
+          ),
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                InkWell(
+                  onTap: (){
+                    setState(() {
+                      api.text = 'Bit.ly';
+                    });
+                    //save user option to session
+                    saveUserApiOption('Bit.ly');
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Bit.ly'),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(height: 1,),
+                InkWell(
+                  onTap: (){
+                    setState(() {
+                      api.text = 'Tinyurl.com';
+                    });
+                    //save user option to session
+                    saveUserApiOption('Tinyurl.com');
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Tinyurl.com'),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(height: 1,),
+                InkWell(
+                  onTap: (){
+                    setState(() {
+                      api.text = 'shorte.st';
+                    });
+                    //save user option to session
+                    saveUserApiOption('Shorte.st');
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('shorte.st'),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(height: 1,),
+                InkWell(
+                  onTap: (){
+                    setState(() {
+                      api.text = 'is.gd';
+                    });
+                    //save user option to session
+                    saveUserApiOption('is.gd');
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('is.gd'),
+                      ],
+                    ),
+                  ),
+                ),
+                Divider(height: 1,),
+                InkWell(
+                  onTap: (){
+                    setState(() {
+                      api.text = 'v.gd';
+                    });
+                    //save user option to session
+                    saveUserApiOption('v.gd');
+                    Navigator.pop(context);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('v.gd'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+
+          ),
+        );
+      }
     );
   }
 }
